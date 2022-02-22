07481667b46418bbf44af18fe5823cab3b88eca2
Alina <phobosmoon2.0@gmail.com>
2022-02-22 17:43:48 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,40 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        boolean isGreenLight = false;
+        boolean isRedLight = true;
+
+        int speedOfPlayer1 = 0;
+        int speedOfPlayer2 = 0;
+        int speedOfPlayer3 = 2;
+
+
+        if (false) {
+            System.out.println("Все игроки могут двигаться");
+        }
+        if (true) {
+            if (speedOfPlayer1 > 0) {
+                System.out.println("1");
+            }
+            if (speedOfPlayer2 > 0) {
+                System.out.println("1");
+            }
+            if (speedOfPlayer3 > 0) {
+                System.out.println("1");
+            }
+        }
+
+    }
+
+
+    // Допишите здесь логику так, что будет подсчитано и выведено
+    // количество игроков, которые выбывают.
+    // Если свет зелёный, то проходят все игроки (0 выбывают).
+    // Если свет красный, то выбывает каждый, чья скорость не 0.
+
+}
+
+
+
+
